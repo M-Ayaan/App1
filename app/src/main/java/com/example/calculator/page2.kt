85dd317f297a6dd1da628cf5b3d847f7316bb10c
M-Ayaan <90791846+M-Ayaan@users.noreply.github.com>
2022-12-08 20:28:00 +0400

    Test

--- app/src/main/java/com/example/calculator/page2.kt
+++ app/src/main/java/com/example/calculator/page2.kt
@@ -10,10 +10,31 @@ class page2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page2)
 val indTxt = findViewById<TextView>(R.id.textInd)
+        val banTxt = findViewById<TextView>(R.id.textBan)
+        val usTxt = findViewById<TextView>(R.id.textUs)
+        val pakTxt = findViewById<TextView>(R.id.textPak)
+        val omanTxt = findViewById<TextView>(R.id.textOman)
+        val phiTxt = findViewById<TextView>(R.id.textPhi)
+        val canTxt = findViewById<TextView>(R.id.textCan)
+
         val intent=getIntent()
         val rate=-intent.getFloatExtra("Rate",0.0f)
-        var ans = rate * -21.1
-        indTxt.text= ans.toString()
+        var ansind = rate * -22.4
+        var ansban = rate * -28.02
+        var ansus = rate * -0.27
+        var anspak = rate * -61.18
+        var ansoman = rate * -0.10
+        var ansphi = rate * -15.06
+        var anscan = rate * -0.37
+
+        indTxt.text= ansind.toString()
+        banTxt.text= ansban.toString()
+        usTxt.text= ansus.toString()
+        pakTxt.text= anspak.toString()
+        omanTxt.text= ansoman.toString()
+        phiTxt.text= ansphi.toString()
+        canTxt.text= anscan.toString()
+
 
     }
 }
